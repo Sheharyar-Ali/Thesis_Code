888fde04393aba234572ce864a18df1d39c83028
Sheharyar Ali <sbinali@tudelft.nl>
2024-09-25 16:54:33 +0200

    Eepy

--- pointGeneration.py
+++ pointGeneration.py
@@ -1,6 +1,9 @@
 import numpy as np
 import itertools
 import pandas as pd
+from scipy.signal import TransferFunction, lsim
+from scipy.integrate import cumtrapz
+
 def generateCoords(fovHor,fovVer,start,end,step):
     """Generate coordiantes within a specific horizontal Fov
 
@@ -16,7 +19,7 @@ def generateCoords(fovHor,fovVer,start,end,step):
     """
     xCoords = np.arange(start,end,step)
     yCoordsMax = np.tan(np.radians(fovHor/2)) * end
-    yCoords = np.arange(-yCoordsMax,yCoordsMax,step)
+    yCoords = np.arange(-yCoordsMax,yCoordsMax,step-2)
     zCoords = np.array([0.2])
     Coords = [xCoords,yCoords,zCoords]
     allCoords = list(itertools.product(*Coords))
@@ -42,35 +45,71 @@ def movement(pointOrigin,dx,theta):
     newOrigin = (Ry @ pointOrigin) - dx
     return newOrigin,newOrigin[0],newOrigin[1],newOrigin[2]
 
-xRange = [1,30]
-step = 2
-u = 0
-theta = np.radians(-5)
-dt = 0.1
-folder = "Heli_Sim/Assets/Scripts/"
+def RealisticMovement(input, MTheta1s,MQ,XTheta1s,g,XU,t):
+    tf_theta1s_theta = TransferFunction([MTheta1s],[1,0])
+    ss_tf_theta1s_theta = MTheta1s/-MQ
+    num_hc2 = (XTheta1s/ ss_tf_theta1s_theta) - g
+    tf_theta_u =  TransferFunction([num_hc2],[1,-XU])
+    
+    _,theta,_ = lsim(tf_theta1s_theta,U=input,T=t)
+    _,u,_ = lsim(tf_theta_u,U=theta,T=t)
+    xPos = cumtrapz(u,t,initial=0)
+    output = pd.DataFrame({})
+    output.insert(0,"Time", t)
+    output.insert(1,"Input",input)
+    output.insert(2,"Theta",theta)
+    output.insert(3,"U",u)
+    output.insert(4,"X",xPos)
+
+    return output
+
+t = np.linspace(0,4,600)
+input = -1/100 * np.sin((1/2 * 3)*np.pi*t)
+MTheta1s = 26.4
+MQ = -1.8954
+MU = 0.05
+XTheta1s = -9.280
+XU = -0.02
+g = 9.80665
+XQ = 0.6674
+behaviour = RealisticMovement(input,MTheta1s,MQ,XTheta1s,g,XU,t)
+behaviour.insert(3,"Theta_deg",np.degrees(behaviour["Theta"]))
+xRange = [4,40]
+step = 8
 coords,oldX,oldY = generateCoords(140,100,xRange[0],xRange[1],step)
-print(coords)
-newCoords =[]
-xChange=[]
-yChange=[]
-zChange=[]
-dx = dx = np.array([u * dt,0,0])
-for point in coords:
-    newPoint,newX,newY,newZ = movement(point,dx,theta)
-    newCoords.append(newPoint)
-    xChange.append(newX)
-    yChange.append(newY)
-    zChange.append(newZ)
-newCoords = np.array(newCoords)
-xChange = np.array(xChange)
-yChange=np.array(yChange)
-zChange=np.array(zChange)
-# print(xChange)
-print(newCoords)
+dfCut =[]
+timestamps = [0.5,1,1.5,2,2.5,3,3.5,4]
+tolerance = 0.005
+folder = "Heli_Sim/Assets/Resources/"
+
 
 coordsOrg = pd.DataFrame({"X":oldY, "Y": np.ones_like(oldY)*0.2 , "Z": oldX})
 coordsOrg.to_csv(folder+"coordsOrg.csv")
-coordsNew = pd.DataFrame({"X": yChange, "Y":zChange, "Z": xChange})
-coordsNew.to_csv(folder+"coordsNew.csv" )
+for T in timestamps:
+    toAdd = behaviour[(behaviour["Time"]>= T - tolerance ) & (behaviour["Time"] < T+tolerance) ]
+    print(toAdd.iloc[0])
+    dfCut.append(toAdd.iloc[0])
+for i,slice in enumerate(dfCut):
+    dx = np.array([slice["U"] * 0.5,0,0])
+    newCoords =[]
+    xChange=[]
+    yChange=[]
+    zChange=[]
+    for point in coords:
+        newPoint,newX,newY,newZ = movement(point,dx,slice["Theta"])
+        newCoords.append(newPoint)
+        xChange.append(newX)
+        yChange.append(newY)
+        zChange.append(newZ)
+    newCoords = np.array(newCoords)
+    xChange = np.array(xChange)
+    yChange=np.array(yChange)
+    zChange=np.array(zChange)
+    # print(xChange)
+    print(newCoords)
+    name = "coordsNew"+ str(timestamps[i])+ ".csv"
+    
+    coordsNew = pd.DataFrame({"X": yChange, "Y":zChange, "Z": xChange})
+    coordsNew.to_csv(folder+name )
 # print(coordsOrg)
 # print(coordsNew)
